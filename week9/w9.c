a6ef33b68edfdae03d13bda0c12f6ca5ac4ccfc5
taeyeong <asdssaa@g.skku.edu>
2026-04-29 18:40:37 +0900

    complete week9

--- week9/w9.c
+++ week9/w9.c
@@ -1,9 +1,12 @@
+#define _GNU_SOURCE
+
 #include <errno.h>
 #include <signal.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/types.h>
 #include <sys/wait.h>
@@ -14,9 +17,10 @@
 volatile sig_atomic_t should_exit = 0;
 int error_code = 0;
 
-#define ACK_TYPE 1000000ULL  // This can be used to separate ack messages from normal messages
+#define ACK_TYPE 1000000L  // This can be used to separate ack messages from normal messages
 
 #define ERASE_CURRENT_LINE "\33[2K\r" // This is a special string (ANSI escape code) that erases the current line in terminal
+#define MSG_DATA_SIZE(msg) (sizeof(msg) - sizeof((msg).msgtype))
 
 typedef struct {
   long msgtype;  // hint: receiver's user_id
@@ -41,26 +45,44 @@ int main() {
   if (should_exit) return error_code;
   int receiver_id = get_id("Receiver ID: ");
   if (should_exit) return error_code;
+  if (user_id <= 0 || receiver_id <= 0) {
+    fprintf(stderr, "User ID and receiver ID must be positive.\n");
+    return 1;
+  }
 
   key_t key;
   int qid;
-  // TODO: create key by "getting it"
+  key = ftok(".", 'a');
+  if (key == -1) {
+    perror("ftok");
+    return 1;
+  }
 
-  // TODO: create message queue by "getting it"
+  qid = msgget(key, IPC_CREAT | 0660);
+  if (qid == -1) {
+    perror("msgget");
+    return 1;
+  }
 
   switch (pid = fork()) {
     case -1:
       perror("fork");
+      msgctl(qid, IPC_RMID, NULL);
+      return 1;
       break;
     case 0:
       // The child is the receiver
       while (should_exit == 0) {
         msgbuf buf;
 
-        // TODO: Receive a message with IPC_NOWAIT
-        if (???) {
+        if (msgrcv(qid, &buf, MSG_DATA_SIZE(buf), user_id, IPC_NOWAIT) != -1) {
           msgbuf_ack ack;
-          ack.msgtype = ACK_TYPE + receiver_id;
+          ack.msgtype = ACK_TYPE + buf.sender_id;
+          time_t t = time(NULL);
+          struct tm tm;
+          localtime_r(&t, &tm);
+          strftime(ack.timestamp, sizeof(ack.timestamp), "%Y-%m-%d %H:%M:%S",
+                   &tm);
 
           printf(ERASE_CURRENT_LINE "User %d:\t%s", buf.sender_id,
                  buf.text);  // This line erases the current line and prints the
@@ -68,13 +90,20 @@ int main() {
           printf("User %d:\t", user_id);
           fflush(stdout);
 
-          // TODO: Send an ack message (current timestamp) to the sender
+          if (msgsnd(qid, &ack, MSG_DATA_SIZE(ack), IPC_NOWAIT) == -1) {
+            if (errno != EIDRM && errno != EINVAL) perror("msgsnd");
+            should_exit = 1;
+            error_code = 1;
+          }
+        } else if (errno != ENOMSG) {
+          if (errno != EIDRM && errno != EINVAL) perror("msgrcv");
+          should_exit = 1;
+          error_code = 1;
         }
 
-        // TODO: Receive ack message using IPC_NOWAIT and store the messeg in
-        // `read_time`
         msgbuf_ack read_time;
-        if (???) {
+        if (msgrcv(qid, &read_time, MSG_DATA_SIZE(read_time),
+                   ACK_TYPE + user_id, IPC_NOWAIT) != -1) {
           // NOTE: The printf statement below clears the current line and then
           // sends a message This printf only works properly when the sender and
           // receiver are running at the same itme Otherwise, it will clear the
@@ -83,6 +112,10 @@ int main() {
                  read_time.timestamp);
           printf("User %d:\t", user_id);
           fflush(stdout);
+        } else if (errno != ENOMSG) {
+          if (errno != EIDRM && errno != EINVAL) perror("msgrcv");
+          should_exit = 1;
+          error_code = 1;
         }
       }
       _exit(error_code);
@@ -107,17 +140,27 @@ int main() {
         }
 
         if (strncmp(line, "quit\n", 5) == 0) {
-          kill(pid, SIGINT);
-          waitpid(pid, NULL, 0);
           free(line);
           break;
         }
 
-        // Your code
+        buf.sender_id = user_id;
+        snprintf(buf.text, sizeof(buf.text), "%s", line);
+        if (msgsnd(qid, &buf, MSG_DATA_SIZE(buf), IPC_NOWAIT) == -1) {
+          perror("msgsnd");
+          should_exit = 1;
+          error_code = 1;
+        }
 
         free(line);
       }
-      // TODO: Remove the message queue
+
+      kill(pid, SIGINT);
+      waitpid(pid, NULL, 0);
+      if (msgctl(qid, IPC_RMID, NULL) == -1 && errno != EINVAL) {
+        perror("msgctl");
+        error_code = 1;
+      }
 
       break;
   }
@@ -131,15 +174,19 @@ void get_input(char** buf, size_t* size, char* unformatted_prompt, ...) {
   va_list args;
   va_start(args, unformatted_prompt);
   char* prompt;
-  if (vasprintf(&prompt, unformatted_prompt, args) == -1) {
+  int ret = vasprintf(&prompt, unformatted_prompt, args);
+  va_end(args);
+  if (ret == -1) {
     perror("vasprintf");
     error_code = 1;
+    should_exit = 1;
     return;
   }
-  va_end(args);
 
   fputs(prompt, stdout);
+  free(prompt);
 
+  errno = 0;
   if (getline(buf, size, stdin) == -1) {
     should_exit = 1;
     if (errno != 0) {
@@ -150,7 +197,7 @@ void get_input(char** buf, size_t* size, char* unformatted_prompt, ...) {
 
 int get_id(char* text) {
   char* input = NULL;
-  size_t len;
+  size_t len = 0;
 
   get_input(&input, &len, text);
 
